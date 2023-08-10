3891feccb6813d1dde9c9013da7ad0c7ad45ed03
miyu <miyu.mine0529@gmail.com>
2023-08-10 11:07:24 +0900

    printをprintlnに変更、Listにsasakiを追加

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,11 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> names = List.of("sato", "suzuki", "tanaka", "takahashi", "ito");
+        List<String> names = List.of("sato", "suzuki", "tanaka", "takahashi", "ito", "sasaki");
         System.out.println(names);
 
-        names.stream().sorted().forEach(System.out::print);
+        names.stream().sorted().forEach(System.out::println
+        );
         //List<String> sortedResult = names.stream().sorted().toList();
         //System.out.println(sortedResult);
 
